7354be1d6394ac879ac75c681178f21c6a134ef8
JorgeCoronilla <jorge.coronilla.naranjo@gmail.com>
2024-03-03 14:28:29 +0100

    [Ux] Footer mobile

--- src/app/ui/components/contact/contact.tsx
+++ src/app/ui/components/contact/contact.tsx
@@ -6,71 +6,77 @@ import InputFields from './inputFields';
 import Modal from '../modal';
 import Warning from './warning';
 import { useContact } from '@/app/lib/hooks/useContact';
+import SocialMedia from '../header/socialMedia';
 
 export const ContactForm = () => {
   const { sendMessage, getData, warning, isOnFocus, showModal } = useContact();
 
   return (
-    <section
-      className={styles.contact}
-      id="contact"
-    >
-      <div className={styles.contactForm}>
-        <form onSubmit={sendMessage}>
-          <InputFields
-            action={getData}
-            label="Nombre:"
-            type="text"
-            name="name"
-            warning={!warning.name && isOnFocus.name}
-            isOnFocus={isOnFocus.name}
-            warningMessage="Escribe un nombre válido"
-          />
-
-          <InputFields
-            action={getData}
-            label="Email:"
-            type="text"
-            name="email"
-            warning={!warning.email && isOnFocus.email}
-            isOnFocus={isOnFocus.email}
-            warningMessage="Escribe un email válido"
-          />
-          <InputFields
-            action={getData}
-            label="Teléfono:"
-            type="text"
-            name="phone"
-            warning={!warning.phone && isOnFocus.phone}
-            isOnFocus={isOnFocus.phone}
-            warningMessage="Escribe un teléfono válido"
-          />
-
-          <div>
-            <label id="text-area-label">Mensaje:</label>
-            <textarea
-              name="message"
-              defaultValue="Hello! your message here"
-              onChange={getData}
-              aria-labelledby="text-area-label"
+    <>
+      <section
+        className={styles.contact}
+        id="contact"
+      >
+        <div className={styles.contactForm}>
+          <form onSubmit={sendMessage}>
+            <InputFields
+              action={getData}
+              label="Nombre:"
+              type="text"
+              name="name"
+              warning={!warning.name && isOnFocus.name}
+              isOnFocus={isOnFocus.name}
+              warningMessage="Escribe un nombre válido"
             />
-          </div>
-          <Warning
-            show={!warning.message && isOnFocus.message}
-            message="Escribe un mensaje válido"
-          />
-          <div>
-            <Button
-              text="Enviar"
-              isDisabled={!warning.allFields}
+
+            <InputFields
+              action={getData}
+              label="Email:"
+              type="text"
+              name="email"
+              warning={!warning.email && isOnFocus.email}
+              isOnFocus={isOnFocus.email}
+              warningMessage="Escribe un email válido"
             />
-          </div>
-        </form>
-        <Modal
-          show={showModal}
-          message="Mensaje enviado"
-        />
-      </div>
-    </section>
+            <InputFields
+              action={getData}
+              label="Teléfono:"
+              type="text"
+              name="phone"
+              warning={!warning.phone && isOnFocus.phone}
+              isOnFocus={isOnFocus.phone}
+              warningMessage="Escribe un teléfono válido"
+            />
+
+            <div>
+              <label id="text-area-label">Mensaje:</label>
+              <textarea
+                name="message"
+                defaultValue="Hello! your message here"
+                onChange={getData}
+                aria-labelledby="text-area-label"
+              />
+            </div>
+            <Warning
+              show={!warning.message && isOnFocus.message}
+              message="Escribe un mensaje válido"
+            />
+            <div>
+              <Button
+                text="Enviar"
+                isDisabled={!warning.allFields}
+              />
+            </div>
+          </form>
+          <Modal
+            show={showModal}
+            message="Mensaje enviado"
+          />
+        </div>
+      </section>
+      <footer>
+        <SocialMedia />
+      </footer>
+    </>
   );
 };
